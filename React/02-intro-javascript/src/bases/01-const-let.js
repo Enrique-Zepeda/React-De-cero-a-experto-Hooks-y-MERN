c521e45bb0a7ffc5e71462e5b400761e6f70af84
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-05-26 16:14:48 -0600

    Const and Let

---
+++ React/02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,16 @@
+//variable y constantes
+
+const nombre = "Kike";
+const apellido = "Zepeda";
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido);
+
+if (true) {
+  const nombre = "Adair";
+  console.log(nombre);
+}
+
+console.log(valorDado);
