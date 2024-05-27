15fdf71c40e081981d3a055fff525607e328835e
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-05-27 06:17:15 -0600

    template strings

---
+++ React/02-intro-javascript/src/bases/02-template-string.js
@@ -0,0 +1,13 @@
+const nombre = "Enrique";
+const apellido = "Zepeda";
+
+// const nombreCompleto = nombre + ' ' + apellido;
+const nombreCompleto = `${nombre} ${apellido}`;
+
+console.log(nombreCompleto);
+
+function getSaludo(nombre) {
+  return "Hola " + nombre;
+}
+
+console.log(`Este es un texto: ${getSaludo(nombre)}`);
