8bf13b910ea8428c7076a761a0a4813fc2c43e22
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-05-29 06:46:58 -0600

    Tarea 1

--- React/03-counter-app-vite/src/main.jsx
+++ React/03-counter-app-vite/src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import HelloWorldApp from "./HelloWorldApp";
+// import HelloWorldApp from "./HelloWorldApp";
 import "./index.css";
+import { CounterApp } from "./CounterApp";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelloWorldApp />
+    <CounterApp value={1} />
   </React.StrictMode>
 );
