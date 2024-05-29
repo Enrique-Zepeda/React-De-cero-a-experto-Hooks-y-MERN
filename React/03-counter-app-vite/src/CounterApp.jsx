7c691550885d1a613e14f26b96c84b441be7f7e4
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-05-29 07:12:17 -0600

    Eventos

--- React/03-counter-app-vite/src/CounterApp.jsx
+++ React/03-counter-app-vite/src/CounterApp.jsx
@@ -1,10 +1,14 @@
 import PropTypes from "prop-types";
 
+const handleClick = (e) => {
+  console.log(e);
+};
 export const CounterApp = ({ value }) => {
   return (
     <>
       <h1>Contador</h1>
       <h2>{value}</h2>
+      <button onClick={handleClick}>+1</button>
     </>
   );
 };
