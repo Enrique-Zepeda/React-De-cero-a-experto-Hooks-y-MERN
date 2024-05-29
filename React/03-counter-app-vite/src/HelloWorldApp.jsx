f868a9d2547105c060bcb1f3fbc3d0b83c2f6222
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-05-29 06:05:24 -0600

    PropTypes

--- React/03-counter-app-vite/src/HelloWorldApp.jsx
+++ React/03-counter-app-vite/src/HelloWorldApp.jsx
@@ -1,15 +1,20 @@
 import { FirstApp } from "./FirstApp";
+import PropTypes from "prop-types";
 
-function HelloWorldApp({ title = "Hola soy kike", subTitle }) {
-  console.log(title);
+function HelloWorldApp({ title, subTitle }) {
   return (
     <>
       <h1>Enrique Zepeda</h1>
       <h2>{title}</h2>
-      <h3>{subTitle + 1}</h3>
+      <h3>{subTitle}</h3>
       <FirstApp />
     </>
   );
 }
+
+HelloWorldApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.number.isRequired,
+};
 
 export default HelloWorldApp;
--- React/03-counter-app-vite/src/main.jsx
+++ React/03-counter-app-vite/src/main.jsx
@@ -4,6 +4,6 @@ import HelloWorldApp from "./HelloWorldApp";
 import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelloWorldApp title="hola soy zepeda" subTitle={12} />
+    <HelloWorldApp title="Licenciada" subTitle={123} />
   </React.StrictMode>
 );
