005782130003d65d93bd54ad90ebddf96952d413
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-05-30 01:47:04 -0600

    Pruebas toBe y toEqual

---
+++ React/03-counter-app-vite/test/base-pruebas/05-funciones.test.js
@@ -0,0 +1,21 @@
+import { getUser, getUsuarioActivo } from "../../src/base-pruebas/05-funciones";
+
+describe("Pruebas en 05-funciones", () => {
+  //   test("getUser debe de retornar un objeto", () => {
+  //     const testUser = {
+  //       uid: "ABC123",
+  //       username: "El_Papi1502",
+  //     };
+  //     const user = getUser();
+  //     console.log(user);
+  //     expect(testUser).toStrictEqual(user);
+  //   });
+  test("getUsuarioActivo debe de retornar un objeto", () => {
+    const nombre = "fernando";
+    const user = getUsuarioActivo(nombre);
+    expect(user).toEqual({
+      uid: "ABC567",
+      username: nombre,
+    });
+  });
+});
