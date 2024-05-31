6f54197e0bf2dc192b699fc4003fbd069ec9627b
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-05-31 09:14:14 -0600

    GifGrid

--- React/04-gif-expert-app/src/GifExpertApp.jsx
+++ React/04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
@@ -31,12 +32,9 @@ export const GifExpertApp = () => {
         onNewCategory={onAddCategory}
       />
 
-      <ol>
-        {categories.map((category) => {
-          return <li key={category}>{category}</li>;
-        })}
-        {/* <li></li> */}
-      </ol>
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
     </>
   );
 };
