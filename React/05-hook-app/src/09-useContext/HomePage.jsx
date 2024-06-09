c5f59272522918e4d7056404532d8ae0d8be772b
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-06-09 08:20:25 -0600

    useContext

--- React/05-hook-app/src/09-useContext/HomePage.jsx
+++ React/05-hook-app/src/09-useContext/HomePage.jsx
@@ -1,7 +1,13 @@
+import { useContext } from "react";
+import { UserContext } from "./context/UserContext";
+
 export function HomePage() {
+  const { user } = useContext(UserContext);
+  console.log(user);
   return (
     <>
       <h1>HomePage</h1>
+      <pre>{JSON.stringify(user, null, 3)}</pre>
     </>
   );
 }
--- React/05-hook-app/src/09-useContext/LoginPage.jsx
+++ React/05-hook-app/src/09-useContext/LoginPage.jsx
@@ -1,7 +1,27 @@
+import { useContext } from "react";
+import { UserContext } from "./context/UserContext";
+
 export function LoginPage() {
+  // tenemos que usar el hook de useContext y entre los parentesis el contexto que deceamos aceder
+  const { user, setUser } = useContext(UserContext);
+  console.log(user);
   return (
     <>
-      <h1>LoginPage</h1>
+      <h1>
+        LoginPage <small>{user?.name}</small>
+      </h1>
+      <pre>{JSON.stringify(user, null, 3)}</pre>
+      {/* <p>{user.id}</p>
+      <p>{user.name}</p>
+      <p>{user.email}</p> */}
+      <button
+        onClick={() =>
+          setUser({ id: 123, name: "Enrique Adair", email: "kike@gmail.com" })
+        }
+        className="btn btn-info rounded-3"
+      >
+        Establecer Usuario
+      </button>
     </>
   );
 }
